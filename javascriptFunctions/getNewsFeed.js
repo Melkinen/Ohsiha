48b121e20272bcf60ee122f14fd003bd0c1600d7
Miska Melkinen <miska.melkinen@student.tut.fi>
2017-04-10 11:54:49 +0300

    haetaan uutisia mtv3 rss feedistä

--- javascriptFunctions/getNewsFeed.js
+++ javascriptFunctions/getNewsFeed.js
@@ -1,5 +1,43 @@
 document.onload(getNews());
 
+// Changes XML to JSON
+function xmlToJson(xml) {
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
 
 function getNews(){
   console.log("getting");
@@ -8,11 +46,30 @@ function getNews(){
     url: 'http://www.mtv.fi/api/feed/rss/uutiset_uusimmat',
     type: 'GET',
     success: function(data) {
+      newsList = document.getElementById("newsList");
       console.log(data)
-    var  xmlDoc = $.parseXML( data );
-    var  xml = $( xmlDoc );
-    var channel = xml.find( "channel" );
-    console.log(channel);
+      //jsonObject = xmlToJson(data);
+      //console.log(jsonObject)
+      var items = data.getElementsByTagName("item");
+      console.log(items);
+      for (item in items){
+        console.log(items[item].children[1].innerHTML);
+
+        var entry = document.createElement('li');
+        var link = document.createElement('a');
+        link.setAttribute('href', items[item].children[1].innerHTML);
+        link.appendChild(document.createTextNode(items[item].children[0].innerHTML));
+        entry.appendChild(link);
+        entry.appendChild(document.createTextNode(items[item].children[7].innerHTML));
+
+
+
+        //entry.appendChild(document.createTextNode(items[item].children[0].innerHTML + items[item].children[1].innerHTML) );
+        newsList.appendChild(entry);
+
+      }
+      var news = items[0].children.getElementsByTagName("item");
+      console.log(news);
 
     }
   });
